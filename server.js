6b15100c78f38a90081ac7dd9e62a139f376fb64
RakshithNM <rakshith_bellare@outlook.com>
2018-11-01 13:26:45 +0530

    Changes to the route

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.get('/about', (req, res) => {
   });
 });
 
-app.get('/djl', (req, res) => {
+app.get('/projects', (req, res) => {
   console.log("within the projects route");
   res.render('project.hbs', {
     projectsPageMessage: "this is my projects page"
